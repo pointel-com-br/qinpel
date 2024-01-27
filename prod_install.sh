9d8a22fe2b7a8159412a65bcabe84dcd7b9a258e
Éverton M. Vieira <emuvi@outlook.com.br>
2024-01-27 13:47:06 -0300

    updates

--- prod_install.sh
+++ prod_install.sh
@@ -6,3 +6,12 @@ cd qin_sunset
 git pull
 ./put_prod.sh
 cd ..
+
+echo "Installing qin_soul"
+if [ ! -d "qin_soul" ]; then
+    git clone https://github.com/pointel-com-br/qin_soul
+fi
+cd qin_soul
+git pull
+./put_prod.sh
+cd ..
