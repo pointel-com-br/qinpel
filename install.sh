4fa190cd717ec76a9744d9eca5620af637f48550
Éverton M. Vieira <emuvi@outlook.com.br>
2024-01-27 13:39:38 -0300

    adds pull

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 if [ ! -d "qin_sunset" ]; then
-  git clone https://github.com/pointel-com-br/qin_sunset
+    git clone https://github.com/pointel-com-br/qin_sunset
+else
+    cd qin_sunset
+    git pull
+    cd ..
 fi
